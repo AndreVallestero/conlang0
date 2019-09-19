f22676412ffd210d9b12111b7d9b238c65acb2af
Andre Vallestero <andrevallestero@gmail.com>
2019-09-18 20:13:28 -0400

    Updated glyph generator to phonemic inventory

--- glyph-generator/app.js
+++ glyph-generator/app.js
@@ -1,24 +1,31 @@
 'use strict';
 
 const TO_RADS = Math.PI / 180,
-	GLYPH_SIZE = 96,
-	GLPH_THICKNESS = GLYPH_SIZE / 10;
+	CONSONANTS = " nptkwlj",
+	VOWELS = "əeɪɔuæiɑ";
 
 var canvas, context,
+	glyphSize, glyphThickness,
 	darkMode = 0;
 
 function main() {
 	canvas = document.getElementById('mainCanvas');
-	canvas.width = window.innerWidth * 0.95;
+	canvas.width = window.innerWidth * 0.98;
 	canvas.height = window.innerHeight * 0.8;
 	
 	context = canvas.getContext('2d');
-	context.lineWidth = GLPH_THICKNESS;
 	
-	document.getElementById('numInput').onchange = draw;
-	document.getElementById('numInput').onkeyup = draw;
+	document.getElementById('textInput').onchange = draw;
+	document.getElementById('textInput').onkeyup = draw;
 	
+	update_size();
 	draw();
+}
+
+function update_size() {
+	glyphSize = parseInt(document.getElementById("sizeInput").value);
+	glyphThickness = glyphSize / 12;
+	context.lineWidth = glyphThickness;
 }
 
 function draw() {
@@ -29,27 +36,48 @@ function draw() {
 	if(darkMode) context.fillStyle = context.strokeStyle = '#FFFFFF';
 	else context.fillStyle = context.strokeStyle = '#000000';
 	
-	
-	let input = document.getElementById('numInput').value;
+	let input = document.getElementById('textInput')
+		.value.replace(/\s+/g, " ").trim();
 	if(!input.length) return;
-	let glyphList = int_to_glyph(parseInt(input));
+	let glyphList = text_to_glyph(input);
 	
-	draw_glyph_list(GLYPH_SIZE, GLYPH_SIZE, glyphList);
-	draw_glyph_list(GLYPH_SIZE, GLYPH_SIZE * 3, glyphList, 1);
-	draw_glyph_list(GLYPH_SIZE, GLYPH_SIZE * 5, glyphList, 0);
+	draw_glyph_list(glyphSize, glyphSize, glyphList);
+	draw_glyph_list(glyphSize, glyphSize * 3.5, glyphList, 1);
+	draw_glyph_list(glyphSize, glyphSize * 6, glyphList, 0);
 }
 
-function int_to_glyph(num) {
-	let numStr = num.toString(8).split('').reverse().join(''),
-		glyphData = [];
-	for (let i = 0; i < numStr.length; ++i)
-		glyphData.push([parseInt(numStr[i]), i, 0]);
-	return glyphData;
+function text_to_glyph(text) {
+	let textList = text.split(" "),
+		glyphList = [];
+	for (let glyphText of textList) {
+		let glyphData = [],
+			vowelIndex = glyphText.search(
+				new RegExp(VOWELS.split("").join("|"), "g"));
+		if(!vowelIndex)
+			glyphData.push(0);
+		
+		for (let i = 0; i < glyphText.length; ++i) {
+			let textChar = glyphText[i];
+			let val = CONSONANTS.indexOf(textChar);
+			if (val < 0 && i == vowelIndex)
+				val = VOWELS.indexOf(textChar);
+			if (val < 0)
+				val = parseInt(textChar);
+			if (isNaN(val))
+				val = 0;
+			glyphData.push(val);
+		}
+		
+		if (glyphData.length < 3)
+			glyphData.push(0);
+		glyphList.push(glyphData);
+	}
+	return glyphList;
 }
 
 function draw_glyph_list(startXPos, startYPos, glyphList, style = 2) {
 	for (let i = 0; i < glyphList.length; ++i)
-		draw_glyph(startXPos + i * GLYPH_SIZE * 1.05,
+		draw_glyph(startXPos + i * glyphSize * 1.05,
 			startYPos,
 			glyphList[i][0],
 			glyphList[i][1],
@@ -59,12 +87,12 @@ function draw_glyph_list(startXPos, startYPos, glyphList, style = 2) {
 
 // xPos (float) x position of glyph
 // yPos (float) y position of glyph
-// node1Val (int) value of left node from 0 to 7, defaults to 0
+// node0Val (int) value of left node from 0 to 7, defaults to 0
+// node1Val (int) value of top node from 0 to 7, defaults to 0
 // node2Val (int) value of right node from 0 to 7, defaults to 0
-// node3Val (int) value of bottom node from 0 to 7, defaults to 0
 // style (int) draw style, 2 is formal, 1 is semi-formal, 0 is informal, defaults to 2
-function draw_glyph(xPos, yPos, node1Val = 0, node2Val = 0, node3Val = 0, style = 2) {
-	let halfGlyphSize = GLYPH_SIZE / 2,
+function draw_glyph(xPos, yPos, node0Val = 0, node1Val = 0, node2Val = 0, style = 2) {
+	let halfGlyphSize = glyphSize / 2,
 		xDelta = Math.sin(60 * Math.PI / 180) * halfGlyphSize,
 		yDelta = Math.cos(60 * Math.PI / 180) * halfGlyphSize;
 			
@@ -72,40 +100,40 @@ function draw_glyph(xPos, yPos, node1Val = 0, node2Val = 0, node3Val = 0, style 
 	if (style) {
 			
 		draw_dot(xPos, yPos) // Center dot
-		draw_dot(xPos, yPos - halfGlyphSize) // Top dot
-		draw_dot(xPos - xDelta, yPos + yDelta) // Left dot
-		draw_dot(xPos + xDelta, yPos + yDelta) // Right dot
+		draw_dot(xPos - xDelta, yPos - yDelta) // Left dot
+		draw_dot(xPos, yPos + halfGlyphSize) // Top dot
+		draw_dot(xPos + xDelta, yPos - yDelta) // Right dot
 		
 		// If formal(2), draw lines aswell
 		if (style == 2) {
-			draw_line(xPos, yPos, xPos, yPos - halfGlyphSize); // Top line
-			draw_line(xPos, yPos, xPos - xDelta, yPos + yDelta); // Top line
-			draw_line(xPos, yPos, xPos + xDelta, yPos + yDelta); // Top line
+			draw_line(xPos, yPos, xPos - xDelta, yPos - yDelta); // Left line
+			draw_line(xPos, yPos, xPos, yPos + halfGlyphSize); // Top line
+			draw_line(xPos, yPos, xPos + xDelta, yPos - yDelta); // Right line
 		}
 	}
 	
-	draw_node(xPos - xDelta, yPos - yDelta, node1Val, 0);
-	draw_node(xPos + xDelta, yPos - yDelta, node2Val, 1);
-	draw_node(xPos, yPos + halfGlyphSize, node3Val, 2);
+	draw_node(xPos - xDelta, yPos + yDelta, node0Val, 0);
+	draw_node(xPos, yPos - halfGlyphSize, node1Val, 1);
+	draw_node(xPos + xDelta, yPos + yDelta, node2Val, 2);
 }
 
 // xPos (float) x position of node
 // yPos (float) y position of node
 // value (int) value of node from 0 to 7, defaults to 0
-// type (int) 0 is left, 1 is right, 2 is bottom
+// type (int) 0 is left, 1 is top, 2 is right
 function draw_node(xPos, yPos, value, type = 0) {
 	if (!value) return;
 	draw_dot(xPos, yPos)
 	
-	let startAngle = -30 + 120 * type;
-	let halfGlyphSize = GLYPH_SIZE / 2;
+	let startAngle = 30 + 120 * type;
+	let halfGlyphSize = glyphSize / 2;
 	
 	if ([1, 4, 5, 7].includes(value))
 		draw_angled_line(xPos, yPos, startAngle, halfGlyphSize);
 	if ([2, 4, 6, 7].includes(value))
-		draw_angled_line(xPos, yPos, startAngle + 60, halfGlyphSize);
+		draw_angled_line(xPos, yPos, startAngle - 60, halfGlyphSize);
 	if ([3, 5, 6, 7].includes(value))
-		draw_angled_line(xPos, yPos, startAngle + 120, halfGlyphSize);
+		draw_angled_line(xPos, yPos, startAngle - 120, halfGlyphSize);
 }
 
 function draw_angled_line(xPos, yPos, angle, length) {
@@ -117,7 +145,7 @@ function draw_angled_line(xPos, yPos, angle, length) {
 
 function draw_dot(xPos, yPos) {
 		context.beginPath();
-		context.arc(xPos, yPos, GLPH_THICKNESS/2, 0, Math.PI * 2, true);
+		context.arc(xPos, yPos, glyphThickness/2, 0, Math.PI * 2, true);
 		context.closePath();
 		context.fill();
 }
